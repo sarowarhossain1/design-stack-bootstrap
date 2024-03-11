58f23af3b3b2aab2f9b817df67d7b24240bedd95
Md.Sarowar Hossain Nayem <121633627+sarowarhossain1@users.noreply.github.com>
2024-03-11 17:08:44 +0600

    first commit

---
+++ map.js
@@ -0,0 +1,141 @@
+let num =[1,3,5,7,8]
+let newArr = num.map(fun)
+
+
+function fun (item){
+           return item * 2
+}
+ 
+console.log(newArr)
+
+// m - for classes that set margin
+// p - for classes that set padding
+// Where sides is one of:
+
+// t - for classes that set margin-top or padding-top
+// b - for classes that set margin-bottom or padding-bottom
+// s - (start) for classes that set margin-left or padding-left in LTR, margin-right or padding-right in RTL
+// e - (end) for classes that set margin-right or padding-right in LTR, margin-left or padding-left in RTL
+// x - for classes that set both *-left and *-right
+// y - for classes that set both *-top and *-bottom
+
+{/* <div class="container mb-5">
+<h2 class="text-center py-3">Bootstrap Input Group</h2>
+<div class="row py-3">
+    <div class="col">
+         <div class="input-group">
+            <div class="input-group-prepend">
+            <span class="input-group-text">$</span>
+            </div>
+             <input type="text" class="form-control">
+         </div>
+        </div>
+   
+    <div class="col ">
+        <div class="input-group mb-3">
+            <input type="text" class="form-control">
+            <div class="input-group-append">
+                <span class="input-group-text">@yaho</span>
+            </div>
+        </div>
+        <div class="input-group input-group-lg mb-3">
+            <input type="text" class="form-control">
+            <div class="input-group-append">
+                <span class="input-group-text">@yaho</span>
+            </div>
+        </div>
+        <div class="input-group input-group-sm mb-3">
+            <input type="text" class="form-control">
+            <div class="input-group-append">
+                <span class="input-group-text">@yaho</span>
+            </div>
+        </div>
+
+        
+    </div>
+</div>
+</div>
+</div> */}
+
+
+
+
+
+
+
+
+
+<div class="container-flug">
+<nav class="nav  row bg-dark text-white  ">
+   <div class="col-3 logo-item navbar-expand-sm">
+     <img  src="./logo.png" alt="">
+   </div>
+   <div class="col-3 search-item navbar-expand-sm">
+       <div class="search-info"> 
+           <input type="text" name="search" placeholder="Search">
+       </div>
+       <!-- <div class="search">
+           <i class="fa-solid fa-magnifying-glass"></i>
+       </div> -->
+   </div>
+   <div class="col-6 d-flex px-3  navbar-expand-sm">
+           <div class="offer-item d-flex">
+               <div class="offer-gift ">
+                   <i class="fa-solid fa-gift gift "></i>
+               </div>
+               <div class="offer">
+                   <p class="offer-e">Offers <br>
+                   <span class="offer-s">Latests Offer</span></p>
+               </div>
+           </div>
+           <div class="bold-item d-flex">
+              <div class="bold-icon">
+               <i class="fa-solid fa-bolt-lightning"></i>
+              </div>
+              <div class="bold">
+               <p class="bold-e">JBL Week<br>
+               <span class="bold-s">Speacial Deal</span></p>
+              </div>
+           </div>
+           <div class="account d-flex">
+                 <div class="ac-account">
+                   <i class="fa-solid fa-user"></i>
+                 </div>
+                 <div class="user">
+                <p class="user-e">Acount <br>
+               <span class="user-s">Register or Login</span></p>
+                 </div>
+           </div>
+           <div class="button">
+             <a href="" class="pc-button">
+               <button >PC Builder</button>
+             </a>
+           </div>
+   </div>
+</nav>
+<!-- navbar -->
+<div class="row">
+  <div class="col-12 Navigation">
+       <div class="bar-info">
+        <a href="#">Desktop</a>
+        <a href="#">Laptop</a>
+        <a href="#">Component</a>
+        <a href="#">Monitor</a>
+        <a href="#">UPS</a>
+        <a href="#">Phone</a>
+        <a href="#">Tablet</a>
+        <a href="#">Office Equipment</a>
+        <a href="#">Camera</a>
+        <a href="#">Security</a>
+        <a href="#">Networking</a>
+        <a href="#">Software</a>
+        <a href="#">Server & Storage</a>
+        <a href="#">Accessories</a>
+        <a href="#">Gadget</a>
+        <a href="#">Gaming</a>
+        <a href="#">TV</a>
+        <a href="#">Applience</a>
+       </div>
+  </div>   
+</div>
+</div>
